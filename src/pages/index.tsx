ed17111ba2a901b32c564c7c34b5be2fb05a7543
fredanup <fredanup@gmail.com>
2025-04-27 10:39:18 -0500

    Se modifica la interfaz de inicio de sesión

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -37,47 +37,68 @@ export default function Home() {
       {status === 'unauthenticated' && (
         <>
           <Image
-            src={'/background.jpg'}
+            src={'/wallpaper.jpg'}
             layout="fill"
             objectFit="cover"
-            alt="Background"
+            alt="Wallpaper"
           />
           <div className="absolute inset-0 bg-black opacity-50 z-10"></div>
           <div className="fixed inset-0 flex items-center justify-center z-30 flex-col">
-            <Image
-              className="absolute top-0 right-0 m-4 p-2"
-              src={'/logo.png'}
-              width={100}
-              height={100}
-              alt="Logo"
-            />
-            <div className="p-12">
-              <div className="text-left mb-6">
-                <h1 className="font-bold text-3xl text-white">PACIFIC</h1>
-                <h1 className="font-bold text-3xl text-black text-outline-white">
-                  SECURITY
-                </h1>
-                <h1 className="font-bold text-3xl text-white">INTERNATIONAL</h1>
+            <div className="bg-white rounded-lg shadow-lg p-9 flex flex-col m-9">
+              <div className="flex flex-row justify-center items-center mt-4">
+                <Image src={'/logo.png'} width={100} height={100} alt="Logo" />
               </div>
-              <p className="text-white text-sm font-light text-justify">
-                Con la finalidad de brindar un excelente servicio y estar más
-                cerca de nuestros beneficiarios contamos con una red de oficinas
-                en la macro región sur del Perú.
-              </p>
-            </div>
-            <div
-              onClick={() => {
-                signIn('google').catch(console.log);
-              }}
-              className="cursor-pointer mt-12 w-64 rounded-full border bg-white px-4 py-2 text-lg font-normal text-black hover:text-white hover:bg-black hover:border-transparent flex flex-row gap-2 items-center justify-center"
-            >
-              <Image
-                src={'/icons/google.png'}
-                width={30}
-                height={30}
-                alt="Logo"
-              />
-              <label className="cursor-pointer">Iniciar sesión</label>
+
+              <div className="flex flex-col gap-6 mt-4 p-2">
+                <h1 className="font-bold text-3xl text-black text-outline-white text-center">
+                  BIENVENIDO
+                </h1>
+                <p className="text-black text-sm font-light text-justify">
+                  Seleccione su forma de inicio de sesión preferido. Puede ser
+                  con su cuenta de Google o Facebook.
+                </p>
+              </div>
+              <div className="flex flex-col justify-center items-center mt-9">
+                <div
+                  onClick={() => {
+                    signIn('google').catch(console.log);
+                  }}
+                  className="cursor-pointer w-64 rounded-full border bg-orange-500 px-4 py-2 text-base font-normal text-white hover:bg-black hover:border-transparent flex flex-row gap-2 items-center justify-center"
+                >
+                  <Image
+                    src={'/icons/google.png'}
+                    width={30}
+                    height={30}
+                    alt="Logo"
+                  />
+                  <label className="cursor-pointer">Continuar con Google</label>
+                </div>
+                <div className="flex items-center w-64 my-3">
+                  <div className="flex-grow h-px bg-gray-300"></div>
+                  <span className="mx-4 text-gray-400 text-sm font-light">
+                    o
+                  </span>
+                  <div className="flex-grow h-px bg-gray-300"></div>
+                </div>
+
+                <div
+                  onClick={() => {
+                    signIn('google').catch(console.log);
+                  }}
+                  className="cursor-pointer w-64 rounded-full border bg-[#145089] px-4 py-2 text-base font-normal text-white hover:bg-black hover:border-transparent flex flex-row gap-2 items-center justify-center"
+                >
+                  <Image
+                    src={'/icons/facebook.png'}
+                    className="fill-white"
+                    width={30}
+                    height={30}
+                    alt="Logo"
+                  />
+                  <label className="cursor-pointer">
+                    Continuar con Facebook
+                  </label>
+                </div>
+              </div>
             </div>
           </div>
         </>
